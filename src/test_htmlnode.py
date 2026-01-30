846ba0dbbe19b1906354b3505817e876580eae60
lil-lucco <lucas.tran117@gmail.com>
2026-01-30 12:23:53 -0500

    add HTMLNode class

---
+++ src/test_htmlnode.py
@@ -0,0 +1,20 @@
+import unittest
+
+from htmlnode import *
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props(self):
+        node = HTMLNode()
+        node.props = {
+            "href": "https://www.google.com",
+            "target": "_blank",
+        }
+        node.props_to_html()
+    def test_no_props(self):
+        node = HTMLNode()
+        node.props = None
+        node.props_to_html()
+
+if __name__ == "__main__":
+    unittest.main()
