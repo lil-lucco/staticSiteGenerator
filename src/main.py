f2f350a16b177b1eaf9478f5164087b687f8dd09
lil-lucco <lucas.tran117@gmail.com>
2026-01-30 11:51:30 -0500

    add TextNode class

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from textnode import *
+def main():
+    print("Felicitations, Malefactors!")
+    test = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    print(test)
+main()
