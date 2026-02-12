2902d45d842781e38c04af3ba444ad8e286b2e6f
lil-lucco <lucas.tran117@gmail.com>
2026-02-12 12:40:00 -0500

    add half baked markdown funcs

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,6 @@
 from textnode import TextType, TextNode
 from leafnode import LeafNode
+import re
 
 def text_node_to_html_node(text_node):
     if text_node.text_type not in TextType:
@@ -19,5 +20,64 @@ def text_node_to_html_node(text_node):
             case TextType.IMAGE:
                 return LeafNode("img", "", text_node.prop["src"])
 
-def split_nodes_delimiter(old_nodes, delimeter, text_type):
-    pass #
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+
+# It takes a list of "old nodes", a delimiter, and a text type. It should return a new list of nodes, 
+# where any "text" type nodes in the input list are (potentially) split into multiple nodes based on the syntax. 
+# For example, given the following input:
+#   ``` node = TextNode("This is text with a `code block` word", TextType.TEXT)
+#       new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)```
+# `new_nodes` becomes:
+#   ```[
+#          TextNode("This is text with a ", TextType.TEXT),
+#          TextNode("code block", TextType.CODE),
+#          TextNode(" word", TextType.TEXT),
+#      ]```
+
+    # Build a brand-new list rather than mutating the input list.
+    result = []
+
+    for old_node in old_nodes:
+        # Only raw text nodes can be split further. Already-formatted nodes pass through.
+        # This keeps nested parsing steps composable (text -> code -> bold -> italics).
+        if old_node.text_type != TextType.TEXT:
+            result.append(old_node)
+            continue
+
+        # Splitting on a delimiter should alternate as:
+        # plain, formatted, plain, formatted, plain, ...
+        # If we get an even number of parts, the markdown is unbalanced.
+        parts = old_node.text.split(delimiter)
+        if len(parts) % 2 == 0:
+            raise ValueError(f"invalid markdown syntax: missing closing '{delimiter}'")
+
+        # Even indexes are plain text, odd indexes are wrapped in `text_type`.
+        for i, part in enumerate(parts):
+            # Skip empty chunks so we don't emit empty TextNodes.
+            if part == "":
+                continue
+            if i % 2 == 0:
+                result.append(TextNode(part, TextType.TEXT))
+            else:
+                result.append(TextNode(part, text_type))
+
+    return result
+
+def extract_markdown_images(text):
+    # takes raw markdown text and returns a list of tuples
+    # each tuple should contain the alt text and the URL of any markdown images
+    # example code:
+    # 
+    # text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+    # print(extract_markdown_images(text))        
+    # [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+    pass
+
+def extract_markdown_links(text):
+    # same as above but with HUYAHH
+    # example code:
+    #
+    # text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+    # print(extract_markdown_links(text))
+    # [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+    pass
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,10 +1,22 @@
 import unittest
 
 from textnode import TextNode, TextType
-from functions import text_node_to_html_node
+from functions import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 
 class TestTextNode(unittest.TestCase):
+    def _assert_with_failure_debug(self, test_name, input_value, result, expected):
+        try:
+            self.assertEqual(result, expected)
+        except AssertionError:
+            print(f"\n[{test_name}]")
+            print(f"input:    {input_value}")
+            print(f"result:   {result}")
+            print(f"expected: {expected}")
+            raise
+        else:
+            print(f"{test_name} TEST PASSED!")
+
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
@@ -20,6 +32,79 @@ class TestTextNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
+
+    # Baseline case from the lesson: one delimited segment in the middle.
+    def test_split_nodes_delimiter_code_single_segment(self):
+        old_nodes = [TextNode("This is text with a `code block` word", TextType.TEXT)]
+        result = split_nodes_delimiter(old_nodes, "`", TextType.CODE)
+        expected = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+        self._assert_with_failure_debug(
+            "test_split_nodes_delimiter_code_single_segment",
+            old_nodes,
+            result,
+            expected,
+        )
+
+    # Non-TEXT nodes should be passed through untouched.
+    def test_split_nodes_delimiter_ignores_non_text_nodes(self):
+        old_nodes = [TextNode("already bold", TextType.BOLD)]
+        result = split_nodes_delimiter(old_nodes, "`", TextType.CODE)
+        self._assert_with_failure_debug(
+            "test_split_nodes_delimiter_ignores_non_text_nodes",
+            old_nodes,
+            result,
+            old_nodes,
+        )
+
+    # Multiple delimiter pairs should alternate plain/formatted/plain correctly.
+    def test_split_nodes_delimiter_multiple_segments(self):
+        old_nodes = [TextNode("start `one` mid `two` end", TextType.TEXT)]
+        result = split_nodes_delimiter(old_nodes, "`", TextType.CODE)
+        expected = [
+            TextNode("start ", TextType.TEXT),
+            TextNode("one", TextType.CODE),
+            TextNode(" mid ", TextType.TEXT),
+            TextNode("two", TextType.CODE),
+            TextNode(" end", TextType.TEXT),
+        ]
+        self._assert_with_failure_debug(
+            "test_split_nodes_delimiter_multiple_segments",
+            old_nodes,
+            result,
+            expected,
+        )
+
+    # Unbalanced markdown should raise, so bad content can be rejected early.
+    def test_split_nodes_delimiter_raises_on_unbalanced_delimiter(self):
+        old_nodes = [TextNode("This has `broken markdown", TextType.TEXT)]
+        test_name = "test_split_nodes_delimiter_raises_on_unbalanced_delimiter"
+        try:
+            with self.assertRaises(ValueError):
+                split_nodes_delimiter(old_nodes, "`", TextType.CODE)
+        except AssertionError:
+            print(f"\n[{test_name}]")
+            print(f"input:    {old_nodes}")
+            print("result:   ValueError was not raised")
+            raise
+        else:
+            print(f"{test_name} TEST PASSED!")
+
+    # example test given by course
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+    
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://www.google.com)"
+        )
+        self.assertListEqual([("link", "https://www.google.com")], matches)
 
 
 if __name__ == "__main__":
